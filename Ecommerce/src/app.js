576b4280eb913178717219582a038f8fffcbfbcd
ChristianRosales <christian.rosales9029@gmail.com>
2021-07-25 21:52:04 -0500

    separando componentes en partials

--- Ecommerce/src/app.js
+++ Ecommerce/src/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var mainRoutes = require('./routes/mainRoutes');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -17,9 +17,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', indexRouter);
+app.use('/', mainRoutes);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- Ecommerce/src/controllers/mainController.js
+++ Ecommerce/src/controllers/mainController.js
@@ -1,6 +1,6 @@
 const controller = {
     index: (req, res) => {
-        res.render('index', { title: 'Express' });
+        return res.render('index');
     }
 }
 
